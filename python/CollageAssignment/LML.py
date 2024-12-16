0dc7d6d2a669923dfd23147ea40f47a161ec26d0
VinodKumarMaurya5649 <vkm5649@gmail.com>
2024-12-16 12:55:51 +0530

    update the LML.py

--- python/CollageAssignment/LML.py
+++ python/CollageAssignment/LML.py
@@ -1,21 +1,28 @@
-# class Book:
-#     def __init__(self,Title,author,Year):
-#         self.title = Title
+# library = []
+# def addbook(db,Title,author,Year):
+#     db.append({"title":Title,"author": author,"year":Year})
 
-#     def display():
-#         pass
+# addbook(library,"abs",'rtx',2050)
+# addbook(library,"dcrvf",'Gtx',3060)
+# addbook(library,"abefr",'Mtx',4060)
 
-# book1 = Book(address,"bookname","auther",2003)
-#database
-library = []
-def addbook(db,Title,author,Year):
-    db.append({"title":Title,"author": author,"year":Year})
+# def display(address):
+#     for book in address:
+#         print(f'title - {book["title"]}')
+# display(library)
+class Book:
+    '''
+    @params - Title,description,author,genre,year
+    1. get varios book details
+    2. to display book details
+    '''
+    def __init__(self,Title,description,author,genre,Year):
+        self.title = Title
+        self.description = description
+        self.author= author
+        self.genre= genre
+        self.year=Year
+    def display(self):
+        return f'title - title'
 
-addbook(library,"abs",'rtx',2050)
-addbook(library,"dcrvf",'Gtx',3060)
-addbook(library,"abefr",'Mtx',4060)
-
-def display(address):
-    for book in address:
-        print(f'title - {book["title"]}')
-display(library)
+book1 = Book(address,"bookname","auther",2003)
