44cc5c566ec5a933dbc6367397d8a8e04a05ba25
VinodKumarMaurya5649 <vkm5649@gmail.com>
2024-12-16 09:34:58 +0530

    D:\coding\python\CollageAssignment\project.py

---
+++ python/CollageAssignment/circle.py
@@ -0,0 +1,7 @@
+class circle:
+    pi = 3.14
+    def __init__(self,r):
+        self.radius = radius 
+        def area(self):
+            return circle.pi *self.radius**2
+c1 =circle(10)
---
+++ python/CollageAssignment/project.py
@@ -0,0 +1,58 @@
+from random import randint
+def main():
+    menu = {1:"roll dice",2:"rockPaperSciessor",3:"exit"}
+    while True:
+        try:
+            usrInput = int(input("choose:-\n 1.roll dice\n 2.Play Rock Paper & Sciessor \n 3.exit\n")) 
+            if usrInput == 1:
+                dice()
+            elif usrInput ==2 :
+                rockPaperScissor()
+            elif usrInput == 3:
+                break
+        except:
+            print("invalid input")
+            break
+def rockPaperScissor():
+    Choices = {1:"rock",2:"paper",3:"Scissor"}
+    score = 0;attempts = 0
+    while True:
+        userChoice = int(input("enter your choice 1:rock,2:paper,3:paper,4: exit\n"))
+        try:
+            if userChoice == 4:
+                if attempts >0:
+                     print(f" Your Score is {score}/{attempts}")
+                     print("thanks for playing.")
+                     break
+                else:
+                    break
+            if userChoice not in {1,2,3}:
+                attempts -= (1)
+            computerChoice = randint(1,3)
+            attempts += 1
+            print(f"you chose:{Choices[userChoice]} and computer chose:{Choices[computerChoice]}")
+            if userChoice == computerChoice:
+                print("its a draw")
+                attempts-=1
+            elif str(userChoice)+str(computerChoice) in ["13","21","32"]:
+                print("you win!")
+                score += 1
+            else:
+                print("you lose")
+        except:
+            print("invalid choice")
+            print(f" Your Current Score is: {score}/{attempts}")
+            break
+def dice():
+    menu = {1:"roll dice",3:"menu"}
+    while True:
+        try:
+            usrInput = int(input("choose:-\n 1.roll dice\n 2.main menu\n")) 
+            if usrInput == 1:
+                print(randint(1,6))
+            elif usrInput == 2:
+                 main()
+                 break
+        except:
+            print("invalid input")
+main()
