95814c5a70b2e73d3fc98c48b38d13a4bcd10be5
VinodKumarMaurya5649 <vkm5649@gmail.com>
2024-12-16 13:00:18 +0530

    Update circle.py

--- python/CollageAssignment/circle.py
+++ python/CollageAssignment/circle.py
@@ -1,7 +1,8 @@
 class circle:
     pi = 3.14
-    def __init__(self,r):
+    def __init__(self,r):  #constructor
         self.radius = radius 
         def area(self):
             return circle.pi *self.radius**2
-c1 =circle(10)
+c1 =circle(10);c2 =circle(20);circle(30)
+# del c3; it delete c3
