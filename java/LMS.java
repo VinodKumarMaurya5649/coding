69f699fc501fe26a103a46682d05a7c0b4b8ac0b
VinodKumarMaurya5649 <vkm5649@gmail.com>
2024-12-16 12:54:07 +0530

    D:\coding\java\LMS.java

--- java/LMS.java
+++ java/LMS.java
@@ -1,1 +1,23 @@
- 
+ class Books{
+    /* @params - Title,description,author,genre,year
+         1. get varios book details
+         2. to display book details  
+    */
+   Book(String title,String author,String genre,int year){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.year = genre;
+   }
+   String getDetails(){
+        return "title" + this.title;
+   }
+        
+    
+ }
+
+ public class LMS {
+    public static void main(String[] args){
+        Books new Books("a","auth","rtyu",1999)
+    }
+ }
