cd81bfeff926b99dee2a74024dffffc3a2116d0a
VinodKumarMaurya5649 <vkm5649@gmail.com>
2024-12-22 19:56:16 +0530

    fix and upates in learning

---
+++ learning/formValidation/test..py
@@ -0,0 +1,11 @@
+def factorial(n):
+    ans=1
+    for i in range(n,1,-1):
+        ans=ans*i
+        return ans
+    print(factorial(10))
+    print(factorial(12))
+
+
+    def fun(x):
+        if x>1:    
