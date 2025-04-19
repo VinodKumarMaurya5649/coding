1b72a342383bdd73dd8b7b0a17b5107da7c56592
VinodKumarMaurya5649 <vkm5649@gmail.com>
2025-04-20 02:49:17 +0530

    multilevel inhertence

--- java/InheritanceLearn.java
+++ java/InheritanceLearn.java
@@ -7,15 +7,22 @@ class InheritanceLearn{
      System.out.println("this is Show Function");
     }
 }
-    class Username extends InheritanceLearn{
+class Username extends InheritanceLearn{
         void display()
         {
             System.out.println("my name is <username>");
         }
-        public static void main(String[] args) {
-            Username ob1 = new Username();
-            ob1.display();
-            ob1.address();
-            ob1.show();
-        }
-} 
+}
+class Base2 extends Username{
+    int a=10;
+    void count(){
+    System.out.println(a++);
+    }
+    public static void main(String[] args) {
+        Base2 ob1 = new Base2();
+        ob1.display();
+        ob1.address();
+        ob1.show();
+        ob1.count();
+    }
+}
