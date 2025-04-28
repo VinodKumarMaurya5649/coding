8b23c139b0cb02e852ae1245c06770b9045b8f27
Vinod Kumar Maurya <99025396+VinodKumarMaurya5649@users.noreply.github.com>
2025-04-28 14:28:44 +0530

    Update ArryInsert.java

--- java/ArryInsert.java
+++ java/ArryInsert.java
@@ -1,3 +1,28 @@
-public ArryInsert{
-
+import java.util.*;
+public class InsertArry{
+ 	public static void main(String arg[]){
+		Scanner sc =new Scanner(System.in);
+		 System.out.println("enter n");
+                 int n= sc.nextInt();
+    		int[] arr = new int[n];
+		System.out.println("enter element");
+		for(int i= 0;i<n;i++){
+			arr[i] = sc.nextInt();
+		}		
+        System.out.println("enter position ");
+			int pos1 = sc.nextInt()-1;
+	   System.out.println("enter element");
+			int ele1 = sc.nextInt();
+		int[]newarr =new int[n+1];
+		for(int i=0;i<pos1;i++){
+			newarr[i] = arr[i];
+		}
+		 newarr[pos1] = ele1;
+		 for (int i = pos1; i < n; i++) {
+            newarr[i + 1] = arr[i];
+        }
+	  for(int i= 0;i<newarr.length;i++){
+			System.out.println(newarr[i]);
+		}
+}
 }
