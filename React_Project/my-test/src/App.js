0baa5bfd306efbba7268627943817794d8acf2e0
VinodKumarMaurya5649 <vkm5649@gmail.com>
2026-06-16 23:45:03 +0530

    just testing

---
+++ React_Project/my-test/src/App.js
@@ -0,0 +1,20 @@
+function MyButton(){
+  return (
+    <div className="MyButton">
+      <button>this is button</button>
+    </div>
+    
+  );
+}
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          this is class
+        </h1>
+        <MyButton/>
+    </div>
+  );
+}
+
+export default App;
